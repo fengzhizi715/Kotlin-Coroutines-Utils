784e3cb151c727966e4ecddecf5294c3aa528628
fengzhizi715 <fengzhizi715@126.com>
2020-01-25 19:30:32 +0800

    update

--- common/src/main/kotlin/com/safframework/kotlin/coroutines/extension/Deferred+Extension.kt
+++ common/src/main/kotlin/com/safframework/kotlin/coroutines/extension/Deferred+Extension.kt
@@ -1,12 +1,11 @@
 package com.safframework.kotlin.coroutines.extension
 
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.*
 
 /**
  *
  * @FileName:
- *          com.aihuishou.recyclemachine.extensions.`Deferred+Extension`
+ *          com.safframework.kotlin.coroutines.extension.`Deferred+Extension`
  * @author: Tony Shen
  * @date: 2019-10-11 10:23
  * @version: V1.0 <描述当前版本功能>
@@ -29,3 +28,28 @@ suspend fun <T> Deferred<T>.awaitOrNull(timeout: Long = 0L) = try {
     System.err.println(e.message)
     null
 }
+
+suspend fun <T, R> Deferred<T>.map(mapper: (T) -> R): Deferred<R> =
+    coroutineScope {
+        async {
+            mapper(this@map.await())
+        }
+    }
+
+suspend fun <K, T : Collection<K>, R> Deferred<T>.flatMap(coroutineStart: CoroutineStart = CoroutineStart.DEFAULT, mapper: (K) -> R): Deferred<Collection<R>> =
+    coroutineScope {
+        async(start = coroutineStart) {
+            val result = mutableListOf<R>()
+            val list = mutableListOf<Deferred<Boolean>>()
+            await().forEach { list.add(async { result.add(mapper(it)) }) }
+            list.forEach { it.await() }
+            return@async result
+        }
+    }
+
+suspend fun <K, T : Collection<K>, R> Deferred<T>.concatMap(coroutineStart: CoroutineStart = CoroutineStart.DEFAULT, mapper: (K) -> R): Deferred<Collection<R>> =
+    coroutineScope {
+        async(start = coroutineStart) {
+            await().map { async { mapper(it) } }.map { it.await() }
+        }
+    }
