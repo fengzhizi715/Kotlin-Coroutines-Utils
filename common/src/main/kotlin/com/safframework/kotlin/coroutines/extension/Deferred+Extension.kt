e46edbe5f310cd5b8e300f424d1429731776b730
fengzhizi715 <fengzhizi715@126.com>
2020-01-30 14:00:54 +0800

    update

--- common/src/main/kotlin/com/safframework/kotlin/coroutines/extension/Deferred+Extension.kt
+++ common/src/main/kotlin/com/safframework/kotlin/coroutines/extension/Deferred+Extension.kt
@@ -52,14 +52,14 @@ suspend fun <T> Deferred<T>.awaitOrNull(timeout: Long = 0L) = try {
     null
 }
 
-suspend fun <T, R> Deferred<T>.map(mapper: mapper<T,R>): Deferred<R> =
+suspend inline fun <T, R> Deferred<T>.map(noinline mapper: mapper<T,R>): Deferred<R> =
     coroutineScope {
         async {
             mapper(this@map.await())
         }
     }
 
-suspend fun <K, T : Collection<K>, R> Deferred<T>.flatMap(coroutineStart: CoroutineStart = CoroutineStart.DEFAULT, mapper: mapper<K,R>): Deferred<Collection<R>> =
+suspend inline fun <K, T : Collection<K>, R> Deferred<T>.flatMap(coroutineStart: CoroutineStart = CoroutineStart.DEFAULT, noinline mapper: mapper<K,R>): Deferred<Collection<R>> =
     coroutineScope {
         async(start = coroutineStart) {
             val result = mutableListOf<R>()
@@ -70,20 +70,20 @@ suspend fun <K, T : Collection<K>, R> Deferred<T>.flatMap(coroutineStart: Corout
         }
     }
 
-suspend fun <K, T : Collection<K>, R> Deferred<T>.concatMap(coroutineStart: CoroutineStart = CoroutineStart.DEFAULT, mapper: mapper<K,R>): Deferred<Collection<R>> =
+suspend inline fun <K, T : Collection<K>, R> Deferred<T>.concatMap(coroutineStart: CoroutineStart = CoroutineStart.DEFAULT, noinline mapper: mapper<K,R>): Deferred<Collection<R>> =
     coroutineScope {
         async(start = coroutineStart) {
             await().map { async { mapper(it) } }.map { it.await() }
         }
     }
 
-suspend fun <T1, T2, R> zip(source1: Deferred<T1>, source2: Deferred<T2>, coroutineStart: CoroutineStart = CoroutineStart.DEFAULT, zipper: zipper<T1, T2, R>): Deferred<R> =
+suspend inline fun <T1, T2, R> zip(source1: Deferred<T1>, source2: Deferred<T2>, coroutineStart: CoroutineStart = CoroutineStart.DEFAULT, noinline zipper: zipper<T1, T2, R>): Deferred<R> =
     coroutineScope {
         async(start = coroutineStart) {
             zipper(source1.await(), source2.await())
         }
     }
 
-suspend fun <T1, T2, R> Deferred<T1>.zipWith(other: Deferred<T2>, coroutineStart: CoroutineStart = CoroutineStart.DEFAULT, zipper: zipper<T1, T2, R>): Deferred<R> {
+suspend inline fun <T1, T2, R> Deferred<T1>.zipWith(other: Deferred<T2>, coroutineStart: CoroutineStart = CoroutineStart.DEFAULT, noinline zipper: zipper<T1, T2, R>): Deferred<R> {
     return zip(this, other, coroutineStart, zipper)
 }
